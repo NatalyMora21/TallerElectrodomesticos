42028457ead7bb1355bfb00095b9297bc6f93c4d
NatalyMora21 <tatamora21gmail.com>
2021-09-06 22:52:20 -0500

    Creacion de funcion generarPedido

---
+++ Controllers/pedidos.js
@@ -0,0 +1,34 @@
+
+let {agregarProducto} =  require("./productos")
+const {facturas}= require('../Db/inventario');
+const objFactura = require('./Models/Factura');
+let generarPedido= () => {
+
+    let terminar= 1;
+
+    let pedido= []
+    while (terminar!=0){
+        //Solicitar al cliente productos que desea agregar a la compra
+
+        //Pedir tipo, consumo y procedencia
+        let tipo;
+        let consumo;
+        let procedencia;
+
+        //Producto
+        let productoPedido= agregarProducto(tipo,consumo,procedencia);
+        pedido.push(productoPedido)
+
+    }
+
+    let nuevaFactura= new objFactura.Factura('#001',new Date, pedido)
+
+    facturas.push(nuevaFactura)
+
+
+
+}
+
+export let prueba = ()=>{
+    console.log(Prueba)
+}
